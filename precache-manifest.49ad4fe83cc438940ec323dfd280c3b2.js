7d25658f2df60889382cfb5f94f95008efd965a6
sauravk7077 <sauravk7077@gmail.com>
2020-09-07 09:57:03 +0530

    added page

---
+++ precache-manifest.49ad4fe83cc438940ec323dfd280c3b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79c981130dcd2dc96c66fe1aa0783b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e4bf352b61b352f21811",
+    "url": "./static/css/main.9c4e5c98.chunk.css"
+  },
+  {
+    "revision": "449819a9e1ee09923107",
+    "url": "./static/js/2.e41d3bb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e41d3bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4bf352b61b352f21811",
+    "url": "./static/js/main.960a9e62.chunk.js"
+  },
+  {
+    "revision": "fd937e7db7f998d968cd",
+    "url": "./static/js/runtime-main.2abe4dc9.js"
+  },
+  {
+    "revision": "bfbf683294230bc5351e0eef87e1f650",
+    "url": "./static/media/ringing.bfbf6832.mp3"
+  }
+]);
